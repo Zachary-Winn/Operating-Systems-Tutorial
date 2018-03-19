3cb03ff32daddfd433cdd3e16267ca092a79df06
LoganDMacDonald <logan.macdonald@uoit.net>
2018-03-19 19:44:39 -0400

    made progress on tut8 q1, findNode function needs finishing and bug with head not keeping right value

--- Tutorial8/1/question1.c
+++ Tutorial8/1/question1.c
@@ -20,42 +20,70 @@ struct proc{
 
 struct proc_tree{
 	struct proc node;
+	struct proc_tree *left;
+	struct proc_tree *right;
 }*head,*current_parent;
 
+struct proc_tree * nodeFind(struct proc_tree *node, char parentName[256]){
+	struct proc_tree * tempNode;
+	
+
+	
+}
 
 int addnode(char parentName[256], char nodeName[256],int nodePriority, int nodeMemory){
 	struct proc_tree *newNode;
 	newNode = (struct proc_tree *) malloc(sizeof(struct proc_tree));
 	if (treesize == 0){
+		printf("creating head\n");
 		//if tree empty, make tree root
 		strcpy(newNode->node.parent,parentName); 
 		strcpy(newNode->node.name,nodeName); 
 		newNode->node.priority = nodePriority;
 		newNode->node.memory = nodeMemory;
+
+		head = newNode;
 	}
 	else{
-		char parentName[256];
 		//find the proper place for the next place
 
-
+		printf("finding node\n");
+		newNode = nodeFind(head,parentName);
+		printf("newNode is: %s\n",newNode->node.name);
 		strcpy(newNode->node.parent,parentName); 
 		strcpy(newNode->node.name,nodeName); 
 		newNode->node.priority = nodePriority;
 		newNode->node.memory = nodeMemory;
 	}
 
-
-
+	newNode->left  = NULL;
+	newNode->right = NULL;
 	++treesize;
+	printf("head value is: %s  treesize:%i\n\n",head->node.name,treesize);
 	return 0;
 }
 
-int main(void){
 
+void iterateAllNodes(struct proc_tree *currentNode){
+	struct proc a = currentNode->node; 
+	printf("parent: %s \nname: %s \npriority: %i \nmemory: %i\n",a.parent,a.name,a.priority,a.memory);
+	if(currentNode->left!=NULL){
+	iterateAllNodes(currentNode->left);
+	}
+	if(currentNode->right!=NULL){
+	iterateAllNodes(currentNode->right);
+	}
+}
+
+
+
+int main(void){
+	printf("starting program\n");
 	FILE *fp;
 	const char delim[2] = ",";
 	char buffer[256], *token, tempParent[256],tempName[256];
 	int c, buffcount=0, tempPriority, tempMemory;
+	printf("opening file\n");
 	fp = fopen("processes_tree.txt","r");
 
 	//for each line
@@ -67,11 +95,11 @@ int main(void){
 		}
 
 		buffcount = 0; //reset buffer counter
-		
+
 		//split line based on comma delimiter
 
 		token = strtok(buffer,delim);
-        
+
 		for(int j = 0;j<4;j++){
 			switch(j){
 				case 0:strcpy(tempParent,token);break;
@@ -79,18 +107,22 @@ int main(void){
 				case 2:tempPriority = atoi(token);break;
 				case 3:tempMemory = atoi(token);break;
 			}
-            token = strtok(NULL,delim);
+			token = strtok(NULL,delim);
 		}
 
 		addnode(tempParent,tempName,tempPriority,tempMemory);
+	
 
-        //clean buffer
-        for(int b =0; b<256;b++){
-            buffer[b] = '\0';
-        }
-    }
-
+		//clean buffer
+		for(int b =0; b<256;b++){
+			buffer[b] = '\0';
+		}
+	}
+	printf("closing file\n");
 	fclose(fp);
 
+	
+	printf("\nprinting nodes\n");
+	iterateAllNodes(head);
 	return 0;
 }
